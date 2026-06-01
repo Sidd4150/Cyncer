f9e6f6032d064bef48f2702fd5d0d8fac2a5c134
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-06-01 12:52:53 -0700

    dashboard backend working

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,6 +1,27 @@
+import prisma from "@/app/lib/prisma"
+
+export default async function Dashboard() {
+
+    const totalProd = await prisma.product.count();
+    const listings = await prisma.listing.findMany();
+    const orders = await prisma.order.findMany();
+
+    const totalStock = listings.reduce((sum, l) => sum + l.quantity, 0);
 
 
-export default function dashboard() {
+    return (
+        < div id="dashboard" >
+            <p>total product count {totalProd}</p>
+            <p>total stock {totalStock}</p>
 
-    return <><h1>This is dashboard</h1></>
+            <ul>
+                {listings.map((listing) => (
+                    <li key={listing.id}>
+                        {listing.platform} - {listing.quantity} in stock
+                    </li>
+                ))}
+            </ul>
+
+        </div >)
+
 }
